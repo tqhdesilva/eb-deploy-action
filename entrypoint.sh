e0db8a3c11e3901beeab1ff34ca493e6b5d3d544
tqhdesilva <tdesilvaqihao@gmail.com>
2019-12-08 13:37:18 -0700

    removed exports from entrypoing

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/sh -l
 
-export AWS_ACCESS_KEY_ID=$1
-export AWS_SECRET_ACCESS_KEY=$2
-export AWS_DEFAULT_REGION=$3
-
 # Set up eb profile
 AWS_CONFIG_FILE=${HOME}/.aws/config
 AWS_PROFILE=eb-cli
